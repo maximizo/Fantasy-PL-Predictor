df7bfa9717b46afb9f8c5f205f2404e483077efc
megancoggs <m.cogguillo@gmail.com>
2021-01-31 18:59:09 -0500

    added tableau link, update folder structure

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ from api.data import data_connector
 app = Flask(__name__, static_url_path='')
 
 
-# serve a dynamic html file from the flask app
+# serve a html file from the flask app
 @app.route('/')
 def index():
     return render_template('html/index.html')
-    # data2=data_connector.get_data2())
 
+@app.route('/machinelearning')
+def machinelearning():
+    return render_template('html/machine_learning.html')
 
 # serve data from the flask app
 @app.route('/data')
 def data1():
     return jsonify(data_connector.get_data())
+
